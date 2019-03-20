236891fa212b7df55398cad22c20d2374adeed9f
Jarred Parr <jparr721@gmail.com>
2019-03-19 20:48:55 -0400

    working on convnet

---
+++ cerebrum/layers/include/conv.h
@@ -0,0 +1,20 @@
+#ifndef CONV_H_
+#define CONV_H_
+
+namespace layer {
+  class Conv {
+    public:
+      Conv(
+
+    private:
+      size_t size;
+      size_t width;
+      size_t depth;
+      size_t filter_size;
+      size_t filter_width;
+      size_t horizontal_stride;
+      size_t vertical_stride;
+  };
+} // namespace layer
+
+#endif // CONV_H_
