cf5ef8b60dc4ff07e09e8536ccdc9e7afe83ad10
Jarred Parr <jparr721@gmail.com>
2019-03-28 10:58:27 -0400

    working on runtime training environment

--- engine/include/runtime.h
+++ engine/include/runtime.h
@@ -1,15 +1,27 @@
 #ifndef RUNTIME_H_
 #define RUNTIME_H_
 
+#include <armadillo>
+#include <vector>
+#include "../../layers/include/base.h"
+
 namespace engine {
   /// The runtime class establishes a running environment for a
   /// neural network
-  template <class NetworkLayerType>
   class Runtime {
     public:
-      Runtime(NetworkLayerType network) : local_net_(network) {};
+      Runtime();
+      Runtime(const layer::Base& layer) {
+        layers.push_back(layer);
+      };
+
+      void add(const layer::Base& layer) {
+        layers.push_back(layer);
+      }
+
+      void fit(const arma::vec& inputs, const std::string optimizer);
     private:
-      NetworkLayerType local_net_;
+      std::vector<layer::Base> layers;
   };
 } // namespace engine
 
