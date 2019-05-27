7bca206314a7732518a5e53004471e8ced82d04c
Jarred Parr <jparr721@gmail.com>
2019-05-26 18:01:28 -0600

    clearing out the old version

--- engine/runtime.cc
+++
@@ -1,8 +0,0 @@
-#include <cassert>
-#include "./include/runtime.h"
-
-namespace engine {
-  void Runtime::fit(const arma::vec& inputs, const std::string optimizer) {
-    assert(layers.size() != 0);
-  }
-} // namespace engine
