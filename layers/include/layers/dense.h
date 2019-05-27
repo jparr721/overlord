a8680a1836be3fbf41a640b0cfb244b9404958a6
Jarred Parr <jparr721@gmail.com>
2019-05-26 21:56:00 -0600

    working on the dense layer attribute to test compilation

---
+++ layers/include/layers/dense.h
@@ -0,0 +1,41 @@
+#ifndef DENSE_H_
+#define DENSE_H_
+
+#include <Eigen/Dense>
+#include <functional>
+#include <layers/base>
+#include <string>
+
+namespace cerebrum {
+  class Dense : public Base {
+    public:
+      Dense(
+          const std::string& activation="relu",
+          const bool use_bias=true,
+          const std::string& kernel_initializer="glorot_uniform",
+          const std::string& bias_initializer="zeros",
+          const std::string& kernel_regularizer=nullptr,
+          const std::string& bias_regularizer=nullptr,
+          const std::string& activity_regularizer=nullptr,
+          const std::string& kernel_constraint=nullptr,
+          const std::string& bias_constraint=nullptr) :
+        kernel_initializer_(kernel_initializer),
+        bias_initializer_(bias_initializer),
+        kernel_regularizer_(kernel_regularizer),
+        bias_regularizer_(bias_regularizer),
+        activity_regularizer_(activity_regularizer),
+        kernel_constraint_(kernel_constraint),
+        bias_constraint_(bias_constraint) {};
+
+    private:
+      std::string kernel_initializer_;
+      std::string bias_initializer_;
+      std::string kernel_regularizer_;
+      std::string bias_regularizer_;
+      std::string activity_regularizer_;
+      std::string kernel_constraint_;
+      std::string bias_constraint_;
+
+} // namespace cerebrum
+
+#endif
