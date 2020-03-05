a0cbffb1d144c811b1eee658d178db6881b3996d
Jarred Parr <jparr721@gmail.com>
2020-03-05 10:21:54 -0800

    snipe old net

--- old/swiss/include/swiss/containers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <eigen3/Eigen/Dense>
-
-namespace cerebrum { namespace swiss {
-  typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> WeightsXf;
-  typedef Eigen::Matrix<float, Eigen::Dynamic, 1> BiasXf;
-} // namespace swiss
-} // namespace cerebrum
